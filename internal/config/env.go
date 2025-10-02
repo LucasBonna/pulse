8499e7c48ebf2d60fe0b7f64a647762869545f5b
LucasBonna <lucasbonnafavaro@gmail.com>
2025-10-01 22:17:03 -0300

    fix: set default port and require TOKEN env variable

--- internal/config/env.go
+++ internal/config/env.go
@@ -13,13 +13,18 @@ type Env struct {
 }
 
 func InitEnvs() *Env {
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal("Error loading .env file")
-	}
+	_ = godotenv.Load()
 
 	port := os.Getenv("PORT")
 	token := os.Getenv("TOKEN")
+
+	if port == "" {
+		port = ":8080" // default
+	}
+	if token == "" {
+		log.Fatal("TOKEN environment variable is required")
+	}
+
 	return &Env{
 		Port:  port,
 		Token: token,
